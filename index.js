e47dde7e590caf923ae82d30a55b26ab35e00058
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-02-27 12:35:59 +0300

    Complete looping and iteration assignments with for and while loops

--- index.js
+++ index.js
@@ -16,17 +16,25 @@
   
   //wrapGifts(gifts);
 
-  function writeCards(names, event) {
-    let messages = []; 
+  //function writeCards(names, event) {
+    //let messages = []; 
   
-    for (let i = 0; i < names.length; i++) { 
-      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
-      messages.push(message);
-    }
+   // for (let i = 0; i < names.length; i++) { 
+     // let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+     // messages.push(message);
+   // }
   
-    return messages; 
-  }
-  function countDown(number) {
+   // return messages; 
+ // }
+ // function countDown(number) {
+   // while (number >= 0) {
+     // console.log(number);
+     // number--; // Decrement the number by 1
+   // }
+  
+
+
+   function countDown(number) {
     while (number >= 0) {
       console.log(number);
       number--; // Decrement the number by 1
@@ -34,3 +42,11 @@
   }
   
   
+  function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  module.exports = { writeCards, countDown };
